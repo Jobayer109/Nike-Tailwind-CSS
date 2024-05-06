c2f38fb556b8ef9400736d07359425ab707ead38
user.email <jobayer.ahmed109@gmail.com>
2024-05-06 21:04:00 +0600

    All sections are created and imported

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,39 @@
+import CustomerReview from "./sections/CustomerReview";
+import Footer from "./sections/Footer";
+import Hero from "./sections/Hero";
+import PopularProducts from "./sections/PopularProducts";
+import Services from "./sections/Services";
+import SpecialOffer from "./sections/SpecialOffer";
+import Subscribe from "./sections/Subscribe";
+import SuperQuality from "./sections/SuperQuality";
+
 const App = () => (
   <main className="relative">
     Nav
     <section className="xl: padding-1 wide:padding-r padding-b">
-      Hero Section
+      <Hero />
     </section>
-    <section className="padding">Popular Products</section>
-    <section className="padding">Super Qualities</section>
-    <section className="padding-x py-10">Services</section>
-    <section className="padding">Special Offers</section>
-    <section className="bg-pale-blue padding">Customer Reviews</section>
-    <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
-    <section className="bg-black padding-x padding-t pb-8">Footer</section>
+    <section className="padding">
+      <PopularProducts />
+    </section>
+    <section className="padding">
+      <SuperQuality />
+    </section>
+    <section className="padding-x py-10">
+      <Services />
+    </section>
+    <section className="padding">
+      <SpecialOffer />
+    </section>
+    <section className="bg-pale-blue padding">
+      <CustomerReview />
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      <Subscribe />
+    </section>
+    <section className="bg-black padding-x padding-t pb-8">
+      <Footer />
+    </section>
   </main>
 );
 
