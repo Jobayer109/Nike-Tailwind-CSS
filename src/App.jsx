2ab99a69223d701aa6d9a693fea87c5b81c29c71
user.email <jobayer.ahmed109@gmail.com>
2024-05-06 21:26:38 +0600

    Importing process modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import CustomerReview from "./sections/CustomerReview";
-import Footer from "./sections/Footer";
-import Hero from "./sections/Hero";
-import PopularProducts from "./sections/PopularProducts";
-import Services from "./sections/Services";
-import SpecialOffer from "./sections/SpecialOffer";
-import Subscribe from "./sections/Subscribe";
-import SuperQuality from "./sections/SuperQuality";
+import {
+  CustomerReview,
+  Footer,
+  Hero,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+} from "./sections/index";
 
 const App = () => (
   <main className="relative">
