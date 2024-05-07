8808f20d876e00d8226ca71c14e25a8d9bed858d
user.email <jobayer.ahmed109@gmail.com>
2024-05-07 12:17:52 +0600

    Header logo set

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,18 @@
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <header>
+      <nav>
+        <a href="/">
+          <img
+            src="/src/assets/images/header-logo.svg"
+            alt="Logo"
+            width={130}
+            height={29}
+          />
+        </a>
+      </nav>
+    </header>
+  );
 };
 
 export default Nav;
